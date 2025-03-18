87a99953721b08f9b4d61eb11659b7d7ab193e3c
Methminpulsara <methminpulsara10@gmail.com>
2025-03-18 16:12:46 +0530

    crate text case

--- src/test/java/edu/icet/ecom/service/impl/CustomerServiceImplTest.java
+++ src/test/java/edu/icet/ecom/service/impl/CustomerServiceImplTest.java
@@ -4,13 +4,15 @@ package edu.icet.ecom.service.impl;
 import edu.icet.ecom.dto.Customer;
 import edu.icet.ecom.entity.CustomerEntity;
 import edu.icet.ecom.repository.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceImplTest {
@@ -24,14 +26,29 @@ public class CustomerServiceImplTest {
     @InjectMocks
     private CustomerServiceImpl service;
 
+    private Customer customer;
+    private CustomerEntity customerEntity;
 
 
 
+    @BeforeEach
+    void setUp() {
+        // Sample test data
+        customer = new Customer();
+        customer.setId(1);
+        customer.setName("John Doe");
 
-        @Test
-        void addCustomer_ShouldMapAndSaveEntity() {
+        customerEntity = new CustomerEntity();
+        customerEntity.setId(1);
+        customerEntity.setName("John Doe");
+    }
 
-        }
+    @Test
+    void testAddCustomer() {
+        when(mapper.map(customer, CustomerEntity.class)).thenReturn(customerEntity);
+        service.add(customer);
+        verify(repository, times(1)).save(customerEntity);
+    }
 
 
 }
